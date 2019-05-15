6e902c79b0c3eee19e5f6979edf4ade87642ba9f
Kirill Pinchuk <cybergrind@gmail.com>
2019-05-16 00:16:20 +0300

    Add initial camera support

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,36 @@
 
 import 'package:flutter/material.dart';
 import './words_widget.dart';
+import './cam_widget.dart';
+import 'package:camera/camera.dart';
 
-void main() => runApp(MyApp());
+/*
+void umain() {
+  //runApp(MyApp());
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+  runApp(TakePictureScreen(camera: null))
+}
+*/
+
+Future<void> main() async {
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen Widget
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
+
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
